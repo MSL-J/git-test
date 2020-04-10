4b09d8f6243629159a8c3dcbbe3f5d3ee71b872b
MSL-J <maldarkmlee@gmail.com>
2020-04-10 14:17:12 +0900

    test

---
+++ test.js
@@ -0,0 +1,8 @@
+let evenNum = (myNum) => {
+    if (myNum % 2 === 0) {
+        return (myNum + " is an even number.");
+    } 
+    else {
+        return (myNum + " is an odd number.");
+    }
+};
